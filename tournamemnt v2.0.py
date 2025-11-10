8aadc5afaa9c24e69c2ca8741042be02a85e10cb
Hatmaker07 <braunholtz-speightN1@lighthouseschool.co.uk>
2025-11-10 14:12:54 +0000

    added constants
    constants are in capital letters (e.g. CONSTANTS)

--- tournamemnt v2.0.py
+++ tournamemnt v2.0.py
@@ -1,10 +1,9 @@
-import array
 import random
-playernum = 6
+SHARE = 6
 p1points = 0
 p2points = 0
-share = 3
-steal = 5
+SHARE = 3
+STEAL = 5
 playerorder = [0,1,2,3,4,5,6]
 player1moves = [0]
 player2moves = [0]
@@ -16,155 +15,154 @@ var13 = 0
 var21 = 0
 var22 = 0
 var23 = 0
-playerdefined = 0
 currentround = 1
 def rules():
     global player1moves, player2moves, currentmove, p1points, p2points
-    if(player1moves[currentmove] == share and player2moves[currentmove] == share):
-        p1points += share
-        p2points += share
-    if(player1moves[currentmove] == steal and player2moves[currentmove] == share):
-        p1points += steal
-    if(player1moves[currentmove] == share and player2moves[currentmove] == steal):
-        p2points += steal
-    if(player1moves[currentmove] == steal and player2moves[currentmove] == steal):
+    if(player1moves[currentmove] == SHARE and player2moves[currentmove] == SHARE):
+        p1points += SHARE
+        p2points += SHARE
+    if(player1moves[currentmove] == STEAL and player2moves[currentmove] == SHARE):
+        p1points += STEAL
+    if(player1moves[currentmove] == SHARE and player2moves[currentmove] == STEAL):
+        p2points += STEAL
+    if(player1moves[currentmove] == STEAL and player2moves[currentmove] == STEAL):
         p1points += 1
         p2points += 1
 def p1verysmart():
     global player1moves, player2moves, var11, var12, var13
     if(currentmove != 1):
-    #stops the forgiving part if they steal first
+    #stops the forgiving part if they STEAL first
         for i in range (-5,0):
-            if(var11 == 0 and player1moves[i] == steal and player2moves[i] == share):
+            if(var11 == 0 and player1moves[i] == STEAL and player2moves[i] == SHARE):
                     var11 = -1
-            if(var11 == 0 and player1moves[i] == share and player2moves[i] == steal):
+            if(var11 == 0 and player1moves[i] == SHARE and player2moves[i] == STEAL):
                     var11 = 1
-        #stops the forgiving part if they steal for 3 moves straight
+        #stops the forgiving part if they STEAL for 3 moves straight
         for i in range (0,3):
-            if(var12 != 3 and player2moves[currentmove - i] == steal):
+            if(var12 != 3 and player2moves[currentmove - i] == STEAL):
                 var12 = var12 + 1
             elif(var12 == 3):
                 var11 = 1
-        #after 5 moves, steal unless they steal
+        #after 5 moves, STEAL unless they STEAL
         for i in range (0,5):
-            if(player2moves[currentmove-i] == share):
+            if(player2moves[currentmove-i] == SHARE):
                 var13 = var13 + 1
             if(var13 == 5):
-                player1moves.append(steal)
+                player1moves.append(STEAL)
             var13 = 0
         #Stops retaliation to retaliation
-        if(player1moves[currentmove - 1] == steal and player2moves[currentmove] == steal and var11 != 1):
-            player1moves.append(share)
+        if(player1moves[currentmove - 1] == STEAL and player2moves[currentmove] == STEAL and var11 != 1):
+            player1moves.append(SHARE)
         else:
-            player1moves.append(share)
+            player1moves.append(SHARE)
 def p2verysmart():
     global player1moves, player2moves, var21, var22, var23
     if(currentmove != 1):
-        #stops the forgiving part if they steal first
+        #stops the forgiving part if they STEAL first
         for i in range (-5,0):
-            if(var21 == 0 and player2moves[i] == steal and player1moves[i] == share):
+            if(var21 == 0 and player2moves[i] == STEAL and player1moves[i] == SHARE):
                 var21 = -1
-            if(var21 == 0 and player2moves[i] == share and player1moves[i] == steal):
+            if(var21 == 0 and player2moves[i] == SHARE and player1moves[i] == STEAL):
                 var21 = 1
-        #stops the forgiving part if they steal for 3 moves straight
+        #stops the forgiving part if they STEAL for 3 moves straight
         for i in range (0,3):
-            if(var22 != 3 and player1moves[currentmove - i] == steal):
+            if(var22 != 3 and player1moves[currentmove - i] == STEAL):
                 var22 = var22 + 1
             elif(var22 == 3):
                 var21 = 1
-        #after 5 moves, steal unless they steal
+        #after 5 moves, STEAL unless they STEAL
         for i in range (0,5):
-            if(player1moves[currentmove-i] == share):
+            if(player1moves[currentmove-i] == SHARE):
                 var23 = var23 + 1
             if(var23 == 5):
-                player2moves.append(steal)
+                player2moves.append(STEAL)
             var23 = 0
             #Stops retaliation to retaliation
-        if(player1moves[currentmove - 1] == steal and player2moves[currentmove] == steal and var11 != 1):
-            player2moves.append(share)
+        if(player1moves[currentmove - 1] == STEAL and player2moves[currentmove] == STEAL and var11 != 1):
+            player2moves.append(SHARE)
         else:
-            player2moves.append(share)
+            player2moves.append(SHARE)
 def p1friendly():
-    player1moves.append(share)
+    player1moves.append(SHARE)
 def p2friendly():
-    player2moves.append(share)
+    player2moves.append(SHARE)
 def p1sample():
     global player2moves, currentmove, var11
     if(currentmove != 1):
-        if(player2moves[currentmove] == share):
-            #returns false if the other player shares
-            player1moves.append(share)
+        if(player2moves[currentmove] == SHARE):
+            #returns false if the other player SHAREs
+            player1moves.append(SHARE)
         else:
-            #has a 8 in 9 chance of returning true if the other player steals
+            #has a 8 in 9 chance of returning true if the other player STEALs
             var11 = random.randint(1,10)
             if(var11 != 1):
-                player1moves.append(steal)
+                player1moves.append(STEAL)
             else:
-                player1moves.append(share)
+                player1moves.append(SHARE)
     else:
-        #shares on the 1st move
-        player1moves.append(share)
+        #SHAREs on the 1st move
+        player1moves.append(SHARE)
 def p2sample():
         global player1moves, currentmove, var21
         if(currentmove != 1):
-            if(player1moves[currentmove] == share):
-                player2moves.append(share)
+            if(player1moves[currentmove] == SHARE):
+                player2moves.append(SHARE)
             else:
                 var21 = random.randint(1,10)
                 if(var21 != 1):
-                    player2moves.append(steal)
+                    player2moves.append(STEAL)
                 else:
-                    player2moves.append(share)
+                    player2moves.append(SHARE)
         else:
-            player2moves.append(share)
+            player2moves.append(SHARE)
 def p1othersample():
-    global var11, player2moves, currentmove, steal
+    global var11, player2moves, currentmove, STEAL
     if(currentmove != 1):
-        if(var11 != 1 and player2moves[currentmove] == steal):
+        if(var11 != 1 and player2moves[currentmove] == STEAL):
             var11 = 1
         if(var11 == 1):
-            player1moves.append(steal)
+            player1moves.append(STEAL)
         else:
-            player1moves.append(share)
+            player1moves.append(SHARE)
     else:
-        player1moves.append(share)
+        player1moves.append(SHARE)
 def p2othersample():
-    global var21, player1moves, currentmove, steal
+    global var21, player1moves, currentmove, STEAL
     if(currentmove != 1):
-        if(var21 != 1 and player1moves[currentmove] == steal):
+        if(var21 != 1 and player1moves[currentmove] == STEAL):
             var21 = 1
         if(var21 == 1):
-            player2moves.append(steal)
+            player2moves.append(STEAL)
         else:
-            player2moves.append(share)
+            player2moves.append(SHARE)
     else:
-        player2moves.append(share)
+        player2moves.append(SHARE)
 def p1forgiveness():
     global player2moves, var11, currentmove
     if(currentmove != 1):
-        if(player2moves[currentmove] == steal):
+        if(player2moves[currentmove] == STEAL):
             var11 = 1
-        if(player2moves[currentmove] == share):
+        if(player2moves[currentmove] == SHARE):
             var11 = 0
         if(var11 == 1):
-            player1moves.append(steal)
+            player1moves.append(STEAL)
         else:
-            player1moves.append(share)
+            player1moves.append(SHARE)
     else:
-        player1moves.append(share)
+        player1moves.append(SHARE)
 def p2forgiveness():
     global player1moves, var21, currentmove
     if(currentmove != 1):
-        if(player1moves[currentmove] == steal):
+        if(player1moves[currentmove] == STEAL):
             var21 = 1
-        if(player1moves[currentmove] == share):
+        if(player1moves[currentmove] == SHARE):
             var21 = 0
         if(var21 == 1):
-            player2moves.append(steal)
+            player2moves.append(STEAL)
         else:
-            player2moves.append(share)
+            player2moves.append(SHARE)
     else:
-        player2moves.append(share)
+        player2moves.append(SHARE)
 pselect1 = []
 pselect2 = []
 score = []
@@ -205,3 +203,4 @@ print("      \         /")
 print("        ------- ")
 print("=======================")
 #points are printed here
+
